17f9aabba52a8f04388c5e0b5567cdaf3c74308e
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2025-07-17 17:47:14 +0530

    Initial commit: Chat App with React and Spring Boot

---
+++ chat-app-backend/src/main/java/com/substring/chat/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.substring.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+
+        config.enableSimpleBroker("/topic");
+        // client ko /topic/messages ko subscribe karna hoga
+        config.setApplicationDestinationPrefixes("/app");
+        // /app/chat
+        // server-side: @MessagingMapping("/chat")
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat") // cpnnection establishment
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
+    }
+    // /chat endpoint par connection a[ka establish hoga
+
+}
